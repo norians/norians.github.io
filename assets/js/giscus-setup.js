9d2eb3d652c2ea9d7ba396bd0449e7cf34411035
norians <96017832+norians@users.noreply.github.com>
2025-12-17 15:23:27 +0000

    Deploying to gh-pages from @ norians/norians.github.io@e56b7f9cd1a408d7aa1ceda164d9a355312ddc07 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "norians/norianna.github.io",
+    "data-repo": "norians/norians.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
